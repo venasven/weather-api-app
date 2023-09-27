e6bd84f847eedd299413b2a66f9a60f858939f99
venasven <honhonlem@gmail.com>
2023-09-26 19:07:42 -0700

    added html structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('goodbye dps')
+const searchForm = document.querySelector(".search-form");
+
+function handleSearchSubmit(e) {
+  e.preventDefault();
+  var inputValue = document.querySelector(".search").value.trim();
+  if (!inputValue) {
+    alert("Please input a valid city name.");
+    return;
+  }
+}
+
+searchForm.addEventListener("submit", handleSearchSubmit);
